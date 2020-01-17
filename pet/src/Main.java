793f56ab8722e851a0fa258f3bee7fed455ec6fb
da4a <dankossss@gmail.com>
2020-01-17 16:31:28 +0200

    Pet

---
+++ pet/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat murzik = new Cat();
+        Dog tyapa = new Dog();
+        Cow zirka = new Cow();
+
+        murzik.voice();
+        tyapa.voice();
+        zirka.voice();
+    }
+}
